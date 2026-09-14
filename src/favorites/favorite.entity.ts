d8ba3a483fc1cd340fb0d10d2154b1acb660a3a2
Diana SIMA <dianasima27@gmail.com>
2026-09-14 17:51:33 +0200

    modification du favoris

--- src/favorites/favorite.entity.ts
+++ src/favorites/favorite.entity.ts
@@ -1,11 +1,17 @@
 import {
   AutoIncrement,
+  BelongsTo,
   Column,
   DataType,
+  ForeignKey,
   Model,
   PrimaryKey,
   Table,
 } from 'sequelize-typescript';
+
+import { User } from '../users/entities/user.entity.js';
+import { Product } from '../products/entities/product.entity.js';
+
 
 @Table({
   tableName: 'favorites',
@@ -16,15 +22,23 @@ export class Favorite extends Model {
   @Column(DataType.INTEGER)
   declare id: number;
 
+  @ForeignKey(() => User)
   @Column({
     type: DataType.INTEGER,
     allowNull: false,
   })
   declare userId: number;
 
+  @BelongsTo(() => User)
+  declare user: User;
+
+  @ForeignKey(() => Product)
   @Column({
     type: DataType.INTEGER,
     allowNull: false,
   })
   declare productId: number;
+
+  @BelongsTo(() => Product)
+  declare product: Product;
 }
--- src/favorites/favorites.module.ts
+++ src/favorites/favorites.module.ts
@@ -3,12 +3,12 @@ import { SequelizeModule } from '@nestjs/sequelize';
 import { Favorite } from './favorite.entity.js';
 import { FavoritesService } from './favorites.service.js';
 import { FavoritesController } from './favorites.controller.js';
+import { User } from '../users/entities/user.entity.js';
+import { Product } from '../products/entities/product.entity.js';
 
 @Module({
-    imports: [
-  SequelizeModule.forFeature([Favorite]),
-],
-    providers: [FavoritesService],
-    controllers: [FavoritesController],
+  imports: [SequelizeModule.forFeature([Favorite, User, Product])],
+  providers: [FavoritesService],
+  controllers: [FavoritesController],
 })
 export class FavoritesModule {}
